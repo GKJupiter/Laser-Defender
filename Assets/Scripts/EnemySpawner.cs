1260a709fc2391249501d2b1c9384d77ef78d4a4
GKJupiter <godkingjupiter+github@gmail.com>
2024-12-05 05:52:12 +0300

    Added timer between waves and enemies

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,22 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] WaveConfigSO currentWave;
+    [SerializeField] List<WaveConfigSO> waveConfigs;
+    [SerializeField] float timeBetweenWaves = 0f;
+    WaveConfigSO currentWave;
     void Start()
     {
-        SpawnEnemies();        
+        StartCoroutine(SpawnEnemyWaves());        
     }
 
-    void SpawnEnemies()
+    IEnumerator SpawnEnemyWaves()
     {
-        for (int i = 0; i < currentWave.GetEnemyCount(); i++)
+        foreach (WaveConfigSO wave in waveConfigs)
         {
-            Instantiate(currentWave.GetEnemyPrefab(i), 
-                        currentWave.GetStartingWaypoint().position,
-                        Quaternion.identity,
-                        transform);
+            currentWave = wave;
+            for (int i = 0; i < currentWave.GetEnemyCount(); i++)
+            {
+                Instantiate(currentWave.GetEnemyPrefab(i), 
+                            currentWave.GetStartingWaypoint().position,
+                            Quaternion.identity,
+                            transform);
+                yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
         
+            }
+            yield return new WaitForSeconds(timeBetweenWaves);
         }   
     }
 
